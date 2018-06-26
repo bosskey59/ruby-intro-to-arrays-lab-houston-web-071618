cb28e63634c968aaa29a93880a3a79a968fb0a3d
Alejandro Aguilar <alejandro.aguilar.jr@gmail.com>
2018-06-26 21:13:07 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -18,4 +18,10 @@ def last_element(array)
   array[-1]
 end
 
-first_element_with_array_methods (array)
+def first_element_with_array_methods (array)
+  array.unshift()
+end
+
+def first_element_with_array_methods (array)
+  array.pop()
+end
